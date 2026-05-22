46239969a41c10a79b73d0d4e05139f8038f5a84
StivenMastrovito <stivenmastrovito6@gmail.com>
2026-05-22 13:57:29 +0200

    fix php.ini

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/phpinfo', function () {
+    return [
+        'max_file_uploads' => ini_get('max_file_uploads'),
+        'upload_max_filesize' => ini_get('upload_max_filesize'),
+        'post_max_size' => ini_get('post_max_size'),
+    ];
+});
